0e64b5ef548e313523032cc0bfd595395ff3a578
Sam Burcksen <sam.burcksen@gmail.com>
2026-02-22 01:37:23 +0100

    Add Radicale and Trilium to NAS

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -5,26 +5,60 @@
 
   imports = with modules; [
     ./hardware.nix
-    ./docker.nix
     common.default
   ];
 
-  # PowerManagement
   powerManagement = {
     enable = true;
     cpuFreqGovernor = "ondemand";
   };
+
+  virtualisation.docker.enable = true;
 
   environment.systemPackages = with pkgs; [
     neovim
     git
   ];
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.settings = {
-    PermitRootLogin = "yes";
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+    };
   };
 
-  networking.firewall.allowedTCPPorts = [ 8123 ];
+  # Self-hosted services
+  services = {
+    # Calendar and Contacts Server
+    radicale = {
+      enable = true;
+      settings = {
+        server.hosts = [ "0.0.0.0:5232" ];
+        storage.filesystem_folder = "/data_ssd/radicale/collections"; 
+        auth = {
+          type = "htpasswd";
+	  htpasswd_filename = "/data_ssd/radicale/users";
+	  htpasswd_encryption = "bcrypt";
+        };
+      };
+    };
+
+    # Note-taking application  
+    trilium-server = {
+      enable = true;
+      dataDir = "/data_ssd/trilium";
+      host = "0.0.0.0";
+      port = 8080;
+    };
+
+    # Media Server
+
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    8123 # HomeAssistant
+    8080 # Trilium
+    5232 # Radicale
+  ];
+
 }
