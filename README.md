ce46e4db5a6b057a6fa20d4b610228e2860c76cb
Sam Burcksen <sam.burcksen@gmail.com>
2026-04-12 23:59:32 +0200

    Update Readme

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # system-configuration
-My system configuration using NixOs, Home-Manager and some regular dotfiles
+My system configuration using NixOs, Home-Manager and some regular dotfiles.
 
 The following hosts are configured:
-- Desktop PC
+- PC
 - Laptop
 - NAS
+
+It is possible to also exculsively use home-manager on a non NixOS system.
+
+### Repository
+
+- [**hosts/**](hosts/)
+  All configured NixOS hosts containing machine specific configuration.
+
+- [**modules/**](modules/)
+  Shared NixOS modules used by the hosts.
+  - [**modules/common**](modules/common)
+  - [**modules/desktop**](modules/desktop)
+  - [**modules/server**](modules/server)
+
+- [**dotfiles/**](dotfiles/)
+  Regular dotfiles used by Home Manager.
+
+- [**home-manager/**](home-manager/)
+  Custom Home Manager modules. Home Manager options are integrated into NixOS modules via the `home` option.
+
--- modules/home.nix
+++ modules/home.nix
@@ -10,19 +10,24 @@
     default = { };
     description = "Home Manager configuration";
   };
+
   imports = [ inputs.home-manager.nixosModules.home-manager ];
-  config.home-manager = {
-    users."sburcksen" = lib.mkAliasDefinitions options.home;
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    extraSpecialArgs = { inherit inputs; };
-  };
-  config.home = {
-    imports = [ ../home-manager ];
 
-    home.username = "sburcksen";
-    home.homeDirectory = "/home/sburcksen";
+  config = {
+    home-manager = {
+      users."sburcksen" = lib.mkAliasDefinitions options.home;
+      useUserPackages = true;
+      useGlobalPkgs = true;
+      extraSpecialArgs = { inherit inputs; };
+    };
 
-    home.stateVersion = config.system.stateVersion;
+    home = {
+      imports = [ ../home-manager ];
+
+      home.username = "sburcksen";
+      home.homeDirectory = "/home/sburcksen";
+
+      home.stateVersion = config.system.stateVersion;
+    };
   };
 }
