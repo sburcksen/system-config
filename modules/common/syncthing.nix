521102af0033554f81c680222eee480d4d194e91
Sam Burcksen <sam.burcksen@gmail.com>
2026-05-01 23:48:40 +0200

    Add Syncthing IDs

--- modules/common/syncthing.nix
+++ modules/common/syncthing.nix
@@ -13,13 +13,13 @@ let
       id = "";
     };
     laptop = {
-      id = "";
+      id = "M7Z6SAT-NC7PYUL-FESMP35-FRI7BMO-VT7P7SX-JFRY2DC-7SVGCEG-BTDZ6A2";
     };
     nas = {
-      id = "";
+      id = "XM7OSNH-GQKUML4-2P57HSC-7THJ4B3-HGVJYUO-XCZYTY4-MVN5UMD-OLQUFAM";
     };
     phone = {
-      id = "";
+      id = "CXYF7AM-MHDEVDL-OGYXE6W-EOYQQEM-DBMJGX7-3WXVS2Z-ZLRQPR4-LHN6OQQ";
     };
   };
 
