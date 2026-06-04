af1bc62e71560fe571dd61f2e779e6bb0d22426d
Sam Burcksen <sam.burcksen@gmail.com>
2026-06-04 18:20:42 +0200

    Configure VSCodium

--- modules/desktop/software/development.nix
+++ modules/desktop/software/development.nix
@@ -25,8 +25,44 @@
       programs.vscode = {
         enable = true;
         package = pkgs.vscodium;
+
+        userSettings = {
+          "editor.formatOnSave" = true;
+          "editor.lineNumbers" = "relative";
+          "editor.cursorSurroundingLines" = 8;
+          "editor.cursorSmoothCaretAnimation" = true;
+
+          "security.workspace.trust.enabled" = false;
+
+          "git.openRepositoryInParentFolders" = "always";
+
+          "haskell.manageHLS" = "PATH";
+        };
+
         profiles.default.extensions = with pkgs.vscode-extensions; [
+          # General
           vscodevim.vim
+
+          # Haskell
+          haskell.haskell
+          justusadam.language-haskell
+
+          # Nix
+          jnoortheen.nix-ide
+
+          # C/C++
+          llvm-vs-code-extensions.vscode-clangd
+
+          # Lua
+          # sumneko.lua
+
+          # Rust
+          rust-lang.rust-analyzer
+
+          # Python
+          #ms-python.python
+          #ms-python.vscode-pylance
+
           #anthropic.claude-code
         ];
       };
