d3cee19e47a663b97d6aca752ec119a5a81bd337
Sam Burcksen <sam.burcksen@gmail.com>
2026-07-18 01:43:49 +0200

    Edit Codium settings

--- modules/desktop/software/development.nix
+++ modules/desktop/software/development.nix
@@ -3,9 +3,7 @@
   lib,
   pkgs,
   ...
-}:
-
-{
+}: {
   options.desktop.devPkgs.enable = lib.mkSubOption config.desktop.enable "development packages";
 
   config = lib.mkIf config.desktop.devPkgs.enable {
@@ -27,21 +25,60 @@
 
         profiles.default = {
           userSettings = {
+            "editor.fontFamily" = "'JetBrainsMono'";
+
+            "editor.tabSize" = 4;
+            "editor.insertSpaces" = true;
+            "editor.detectIndentation" = true;
+
             "editor.formatOnSave" = true;
             "editor.lineNumbers" = "relative";
-            "editor.cursorSurroundingLines" = 8;
-            "editor.cursorSmoothCaretAnimation" = true;
 
+            "editor.minimap.enabled" = false;
+            "editor.guides.bracketPairs" = true;
+            "editor.inlineSuggest.enabled" = true;
+
+            "editor.cursorSurroundingLines" = 8;
+            "editor.cursorSmoothCaretAnimation" = "on";
+            "editor.smoothScrolling" = true;
+
+            "files.trimTrailingWhitespace" = true;
+            "files.insertFinalNewline" = true;
+
+            "workbench.colorTheme" = "Catppuccin Mocha";
+            "workbench.iconTheme" = "catppuccin-mocha";
+            "workbench.startupEditor" = "none";
+            "workbench.editor.enablePreview" = false;
+
+            "breadcrumbs.enabled" = true;
             "security.workspace.trust.enabled" = false;
 
             "git.openRepositoryInParentFolders" = "always";
+            "git.autofetch" = true;
+            "git.confirmSync" = false;
 
+            "telemetry.telemetryLevel" = "off";
+
+            "extensions.autoUpdate" = false;
+            "extensions.autoCheckUpdates" = false;
+
+            # Extensions
             "haskell.manageHLS" = "PATH";
+
+            "vim.useSystemClipboard" = true;
+            "vim.hlsearch" = true;
+            "vim.incsearch" = true;
+            "vim.leader" = "<space>";
+            "vim.camelCaseMotion.enable" = true;
+            "vim.visualstar" = true;
           };
 
           extensions = with pkgs.vscode-extensions; [
             # General
             vscodevim.vim
+            esbenp.prettier-vscode
+            catppuccin.catppuccin-vsc
+            catppuccin.catppuccin-vsc-icons
 
             # Haskell
             haskell.haskell
@@ -60,7 +97,7 @@
             rust-lang.rust-analyzer
 
             # Python
-            #ms-python.python
+            ms-python.python
             #ms-python.vscode-pylance
 
             #anthropic.claude-code
